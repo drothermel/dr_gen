b2a95534cd9bb8024de6920e1eb0fc43bb5a062e
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-01-30 15:01:52 -0500

    added data.py from PyCIL with CIFAR datasets

---
+++ src/dr_gen/utils/data.py
@@ -0,0 +1,63 @@
+# From PyCIL/utils/data.py with modifications
+
+import numpy as np
+from torchvision import datasets, transforms
+
+class iData(object):
+    train_trsf = []
+    test_trsf = []
+    common_trsf = []
+
+
+class iCIFAR10(iData):
+    use_path = False
+    train_trsf = [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ColorJitter(brightness=63 / 255),
+        transforms.ToTensor(),
+    ]
+    test_trsf = [transforms.ToTensor()]
+    common_trsf = [
+        transforms.Normalize(
+            mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)
+        ),
+    ]
+
+    def download_data(self):
+        train_dataset = datasets.cifar.CIFAR10("./data", train=True, download=True)
+        test_dataset = datasets.cifar.CIFAR10("./data", train=False, download=True)
+        self.train_data, self.train_targets = train_dataset.data, np.array(
+            train_dataset.targets
+        )
+        self.test_data, self.test_targets = test_dataset.data, np.array(
+            test_dataset.targets
+        )
+
+class iCIFAR100(iData):
+    use_path = False
+    train_trsf = [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=63 / 255),
+        transforms.ToTensor()
+    ]
+    test_trsf = [transforms.ToTensor()]
+    common_trsf = [
+        transforms.Normalize(
+            mean=(0.5071, 0.4867, 0.4408), std=(0.2675, 0.2565, 0.2761)
+        ),
+    ]
+
+    class_order = np.arange(100).tolist()
+
+    def download_data(self):
+        train_dataset = datasets.cifar.CIFAR100("./data", train=True, download=True)
+        test_dataset = datasets.cifar.CIFAR100("./data", train=False, download=True)
+        self.train_data, self.train_targets = train_dataset.data, np.array(
+            train_dataset.targets
+        )
+        self.test_data, self.test_targets = test_dataset.data, np.array(
+            test_dataset.targets
+        )
+
