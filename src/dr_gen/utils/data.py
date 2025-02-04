aa7a89baf6877d13095405b6680e4cc57e21a336
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-04 18:24:32 -0500

    replace pycil code with torchvision recommended

--- src/dr_gen/utils/data.py
+++ src/dr_gen/utils/data.py
@@ -1,63 +1,31 @@
-# From PyCIL/utils/data.py with modifications
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import v2 as transforms_v2
 
-import numpy as np
-from torchvision import datasets, transforms
+AVAIL_DATASETS = {'cifar10', 'cifar100'}
 
-class iData(object):
-    train_trsf = []
-    test_trsf = []
-    common_trsf = []
+def get_dataset(data_cfg, paths_cfg, train=True):
+    data_name_lower = data_cfg.name.lower()
+    assert data_name_lower in AVAIL_DATASETS
 
-
-class iCIFAR10(iData):
-    use_path = False
-    train_trsf = [
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.ColorJitter(brightness=63 / 255),
-        transforms.ToTensor(),
-    ]
-    test_trsf = [transforms.ToTensor()]
-    common_trsf = [
-        transforms.Normalize(
-            mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)
-        ),
-    ]
-
-    def download_data(self):
-        train_dataset = datasets.cifar.CIFAR10("./data", train=True, download=True)
-        test_dataset = datasets.cifar.CIFAR10("./data", train=False, download=True)
-        self.train_data, self.train_targets = train_dataset.data, np.array(
-            train_dataset.targets
+    if data_name_lower == 'cifar10':
+        return datasets.CIFAR10(
+            root=paths_cfg.dataset_cache_root,
+            train=train,
+            download=data_cfg.download,
         )
-        self.test_data, self.test_targets = test_dataset.data, np.array(
-            test_dataset.targets
+    elif data_name_lower == 'cifar100':
+        return datasets.CIFAR100(
+            root=paths_cfg.dataset_cache_root,
+            train=train,
+            download=data_cfg.download,
         )
 
-class iCIFAR100(iData):
-    use_path = False
-    train_trsf = [
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=63 / 255),
-        transforms.ToTensor()
-    ]
-    test_trsf = [transforms.ToTensor()]
-    common_trsf = [
-        transforms.Normalize(
-            mean=(0.5071, 0.4867, 0.4408), std=(0.2675, 0.2565, 0.2761)
-        ),
-    ]
 
-    class_order = np.arange(100).tolist()
+        
 
-    def download_data(self):
-        train_dataset = datasets.cifar.CIFAR100("./data", train=True, download=True)
-        test_dataset = datasets.cifar.CIFAR100("./data", train=False, download=True)
-        self.train_data, self.train_targets = train_dataset.data, np.array(
-            train_dataset.targets
-        )
-        self.test_data, self.test_targets = test_dataset.data, np.array(
-            test_dataset.targets
-        )
+        
+    
+    
 
