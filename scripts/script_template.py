d2fb1ad6fc500ee9cee696832cc1f51b76eefe05
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-04 18:24:50 -0500

    tried to use script template to test loading dataset, no torch

--- scripts/script_template.py
+++ scripts/script_template.py
@@ -2,6 +2,8 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import logging
 
+from dr_gen.utils import data
+
 
 # Potentially add all of this to dr_util
 def cfg_to_loggable_lines(cfg):
@@ -25,6 +27,11 @@ def log_cfg(cfg):
 def run(cfg: DictConfig):
     log_cfg(cfg)
     logging.info(">> Welcome to your new script! here")
+
+    logging.info(" :: Loading Train Dataset :: ")
+    train_data = data.get_dataset(cfg.data, cfg.paths, train=True)
+    test_data = data.get_dataset(cfg.data, cfg.paths, train=False)
+    logging.info(f">> Downloaded to: {cfg.paths.dataset_cache_root}")
 
 
 if __name__ == "__main__":
