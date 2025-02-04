15c2024de8cc6f53a14867588704f1202bf70040
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-04 18:27:05 -0500

    rye fmt

--- src/dr_gen/utils/data.py
+++ src/dr_gen/utils/data.py
@@ -3,29 +3,22 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import v2 as transforms_v2
 
-AVAIL_DATASETS = {'cifar10', 'cifar100'}
+AVAIL_DATASETS = {"cifar10", "cifar100"}
+
 
 def get_dataset(data_cfg, paths_cfg, train=True):
     data_name_lower = data_cfg.name.lower()
     assert data_name_lower in AVAIL_DATASETS
 
-    if data_name_lower == 'cifar10':
+    if data_name_lower == "cifar10":
         return datasets.CIFAR10(
             root=paths_cfg.dataset_cache_root,
             train=train,
             download=data_cfg.download,
         )
-    elif data_name_lower == 'cifar100':
+    elif data_name_lower == "cifar100":
         return datasets.CIFAR100(
             root=paths_cfg.dataset_cache_root,
             train=train,
             download=data_cfg.download,
         )
-
-
-        
-
-        
-    
-    
-
