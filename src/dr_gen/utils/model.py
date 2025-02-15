ce71635b4528531ab5f0ab21b3b88d6a9255265d
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-14 22:10:36 -0500

    add train_epoch and move optim to model.py for checkpointing reasons

--- src/dr_gen/utils/model.py
+++ src/dr_gen/utils/model.py
@@ -9,24 +9,131 @@ def create_model(cfg, num_classes):
         weights=cfg.model.weights,
         num_classes=num_classes,
     )
-    if cfg.model.load_checkpoint is not None:
-        print(f">> Loading checkpoint: {cfg.model.load_checkpoint}")
+    return model
+
+# Simplified from https://github.com/pytorch/vision/blob/main/references/classification/utils.py#L405
+def set_weight_decay(
+    model,
+    weight_decay,
+):
+    norm_classes = (
+        torch.nn.modules.batchnorm._BatchNorm,
+        torch.nn.LayerNorm,
+        torch.nn.GroupNorm,
+        torch.nn.modules.instancenorm._InstanceNorm,
+        torch.nn.LocalResponseNorm,
+    )
+    params = []
+    def _add_params(module, prefix=""):
+        for name, p in module.named_parameters(recurse=False):
+            if not p.requires_grad:
+                continue
+            params.append(p)
+
+        for child_name, child_module in module.named_children():
+            child_prefix = f"{prefix}.{child_name}" if prefix != "" else child_name
+            _add_params(child_module, prefix=child_prefix)
+
+    _add_params(model)
+
+    param_groups = []
+    for key in params:
+        if len(params[key]) > 0:
+            param_groups.append({"params": params[key], "weight_decay": params_weight_decay[key]})
+    return param_groups
+
+def create_optim_lrsched(cfg, model):
+    opt_name = cfg.optim.name.lower()
+    assert apt_name in OPTIMIZERS, f"Invalid optimizer {cfg.optim.name}."
+    
+    params = set_weight_decay(model, cfg.optim.weight_decay)
+
+    if opt_name.startswith("sgd"):
+        optimizer = torch.optim.SGD(
+            params,
+            lr=cfg.optim.lr,
+            momentum=cfg.optim.momentum,
+            weight_decay=cfg.optim.weight_decay,
+            nesterov=cfg.optim.nesterov,
+        )
+    elif opt_name == "rmsprop":
+        optimizer = torch.optim.RMSprop(
+            params,
+            lr=cfg.optim.lr,
+            momentum=cfg.optim.momentum,
+            weight_decay=cfg.optim.weight_decay,
+            eps=cfg.optim.eps,
+            alpha=cfg.optim.alpha,
+        )
+    elif opt_name == "adamw":
+        optimizer = torch.optim.AdamW(
+            params, lr=cfg.optim.lr, weight_decay=args.weight_decay
+        )
+    else:
+        assert False, f"Invalid optimizer {cfg.optim.name}.")
+
+    assert cfg.optim.lr_scheduler in LR_SCHEDULERS
+    if cfg.optim.lr_scheduler == "steplr":
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=cfg.optim.lr_step_size,
+            gamma=cfg.optim.lr_gamma,
+        )
+    elif cfg.optim.lr_scheduler == "cosineannealinglr":
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=cfg.epochs - cfg.optim.lr_warmup_epochs,
+            eta_min=cfg.optim.lr_min
+        )
+    elif cfg.optim.lr_scheduler == "exponentiallr":
+        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            optimizer,
+            gamma=cfg.optim.lr_gamma
+        )
+    else:
+        assert False, f"Invalid LRSched {cfg.optim.lr_scheduler}."
+    return optimizer, lr_scheduler
+
+def get_model_optim_lrsched(cfg, num_classes):
+    model = create_model(cfg, num_classes)
+    if cfg.load_checkpoint is not None:
+        print(f">> Loading checkpoint: {cfg.load_checkpoint}")
         checkpoint = torch.load(
-            cfg.model.load_checkpoint,
+            cfg.load_checkpoint,
             map_location="cpu",
             weights_only=False,
         )
         model.load_state_dict(checkpoint["model"])
+        optimizer = checkpoint.get("optimizer", None)
+        lr_scheduler = checkpoint.get("lr_scheduler", None)
+
     model.to(cfg.device)
-    return model
+    if optimizer is None or lr_scheduler is None:
+        optimizer, lr_scheduler = create_optim_lrsched(cfg, model)
+
+    return model, optimizer, lr_scheduler
 
 
-def checkpoint_model(cfg, model, checkpoint_name):
-    chpt_dir = Path(cfg.model.write_checkpoint)
+def get_criterion(cfg):
+    crit = cfg.optim.loss.lower()
+    assert crit in CRITERIONS
+    return torch.nn.CrossEntropyLoss(
+        label_smoothing=cfg.optim.label_smoothing,
+    )
+
+
+def checkpoint_model(cfg, model, checkpoint_name, optim=None, lrsched=None):
+    if cfg.write_checkpoint is None:
+        return
+
+    chpt_dir = Path(cfg.write_checkpoint)
     chpt_dir.mkdir(parents=True, exist_ok=True)
     chpt_path = chpt_dir / f"{checkpoint_name}.pt"
-    torch.save(
-        {"model": model.state_dict()},
-        chpt_path,
-    )
+    chpt = {k: v for k, v in [ 
+        ("model", model.state_dict()),
+        ("optimizer", optim),
+        ("lr_scheduler", lrsched),
+    ]}
+    torch.save(chpt, chpt_path)
     print(f">> Saved checkpoint to: {chpt_path}")
+
--- src/dr_gen/utils/train_eval.py
+++ src/dr_gen/utils/train_eval.py
@@ -1,71 +1,75 @@
 from collections import defaultdict
+import time
 
 import torch
+import dr_gen.utils.evaluate as eu
 
 CRITERIONS = {"cross_entropy"}
+OPTIMIZERS = {"sgd", "rmsprop", "adamw"}
+LR_SCHEDULERS = {"steplr", "cosineannealinglr", "exponentiallr"}
 
 
-def get_criterion(cfg):
-    crit = cfg.optim.loss.lower()
-    assert crit in CRITERIONS
-    return torch.nn.CrossEntropyLoss(
-        label_smoothing=cfg.optim.label_smoothing,
-    )
+def update_metrics(metrics, batch_size, loss, output, target, prefix=""):
+    # Calc and Add Metrics
+    acc1, acc5 = eu.accuracy(output, target, topk=(1, 5))
+    img_ps = -1
+    t_key = f"{prefix}_update_time"
+    metrics[t_key].append(time.time())
+    if len(metrics[t_key]) > 1:
+        diff_t = metrics[t_key][-1] - metrics[t_key][-2]
+        img_ps = batch_size * 1.0 / diff_t
+
+    metrics[f"{prefix}_batch_size"].append(batch_size)
+    metrics[f"{prefix}_loss"].append(loss.item())
+    metrics[f"{prefix}_acc1"].append(acc1.item())
+    metrics[f"{prefix}_acc5"].append(acc5.item())
+    metrics[f"{prefix}_img_per_s"].append(img_ps)
 
 
-def get_optim(cfg, model):
-    params = utils.set_weight_decay(model, cfg.optim.weight_decay)
+    # Logging
+    n = sum(metrics[f"{prefix}_batch_size"])
+    print(f">> {prefix} {n}| {loss=} {acc1=} {acc5=} {img_ps=}")
 
-    opt_name = cfg.optim.name.lower()
-    if opt_name.startswith("sgd"):
-        optimizer = torch.optim.SGD(
-            params,
-            lr=cfg.optim.lr,
-            momentum=cfg.optim.momentum,
-            weight_decay=cfg.optim.weight_decay,
-            nesterov=cfg.optim.nesterov,
+
+def agg_metrics(metrics):
+    agg_m = {}
+
+    # Scale and sum
+    bs_key = [k for k in metrics.keys() if 'batch_size' in k][0]
+    for k, v in metrics.items():
+        agg_m[k] = 0
+        for i, vv in enumerate(v):
+            agg_m[k] += vv * metrics[bs_key][i]
+
+    # Normalize
+    total_bs = sum(metrics[bs_key])
+    for k in agg_m.keys():
+        agg_m[k] = agg_m[k] / total_bs
+
+    return agg_m
+
+
+def train_epoch(cfg, epoch, model, dataloader, criterion, optimizer):
+    model.train()
+
+    metrics = defaultdict(list)
+    for i, (image, target) in enumerate(dataloader):
+        start_time = time.time()
+        image, target = image.to(cfg.device), target.to(cfg.device)
+        output = model(image)
+        loss = criterion(output, target)
+        optimizer.zero_grad()
+        loss.backward()
+        if cfg.optim.clip_grad_norm is not None:
+            nn.utils.clip_grad_norm_(
+                model.paramteres(),
+                cfg.optim.clip_grad_norm,
+            )
+        optimizer.step()
+        update_metrics(
+            metrics, image.shape[0], loss, output, target, "t",
         )
-    elif opt_name == "rmsprop":
-        optimizer = torch.optim.RMSprop(
-            params,
-            lr=cfg.optim.lr,
-            momentum=cfg.optim.momentum,
-            weight_decay=cfg.optim.weight_decay,
-            eps=cfg.optim.eps,
-            alpha=cfg.optim.alpha,
-        )
-    elif opt_name == "adamw":
-        optimizer = torch.optim.AdamW(
-            params, lr=cfg.optim.lr, weight_decay=args.weight_decay
-        )
-    else:
-        assert False, f"Invalid optimizer {cfg.optim.name}.")
-
-    assert cfg.optim.lr_scheduler.lower() == cfg.optim.lr_scheduler
-    if cfg.optim.lr_scheduler == "steplr":
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer,
-            step_size=cfg.optim.lr_step_size,
-            gamma=cfg.optim.lr_gamma,
-        )
-    elif cfg.optim.lr_scheduler == "cosineannealinglr":
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer,
-            T_max=cfg.epochs - cfg.optim.lr_warmup_epochs,
-            eta_min=cfg.optim.lr_min
-        )
-    elif cfg.optim.lr_scheduler == "exponentiallr":
-        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-            optimizer,
-            gamma=cfg.optim.lr_gamma
-        )
-    else:
-        assert False, f"Invalid LRSched {cfg.optim.lr_scheduler}."
-
-    # TODO: Handle resume (line 341)
-    # TODO: Handle test_only (line 353)
-
-    return optimizer, lr_scheduler
+    return metrics
 
 
 def eval_model(cfg, model, dataloader, criterion):
@@ -80,44 +84,53 @@ def eval_model(cfg, model, dataloader, criterion):
             target = target.to(cfg.device, non_blocking=True)
             output = model(image)
             loss = criterion(output, target)
-            batch_size = image.shape[0]
+            update_metrics(
+                metrics, image.shape[0], loss, output, target,
+            )
 
-            # Logging
-            metrics["batch_size"].append(batch_size)
-            metrics["loss"].append(loss.item())
-            num_processed_samples += batch_size
-            print(f">> {num_processed_samples}| {loss=}")
+            acc1, acc5 = eu.accuracy(output, target, topk=(1, 5))
+            batch_size = image.shape[0]
+            update_metrics(
+                metrics, batch_size, loss, output, target,
+            )
+
 
     return metrics
 
-# Simplified from https://github.com/pytorch/vision/blob/main/references/classification/utils.py#L405
-def set_weight_decay(
-    model,
-    weight_decay,
-):
-    norm_classes = (
-        torch.nn.modules.batchnorm._BatchNorm,
-        torch.nn.LayerNorm,
-        torch.nn.GroupNorm,
-        torch.nn.modules.instancenorm._InstanceNorm,
-        torch.nn.LocalResponseNorm,
-    )
-    params = []
-    def _add_params(module, prefix=""):
-        for name, p in module.named_parameters(recurse=False):
-            if not p.requires_grad:
-                continue
-            params.append(p)
 
-        for child_name, child_module in module.named_children():
-            child_prefix = f"{prefix}.{child_name}" if prefix != "" else child_name
-            _add_params(child_module, prefix=child_prefix)
+def train_loop(cfg, model, train_dl, val_dl=None):
+    # Things in main
+    # Setup logging here or elsewhere?
+    # cfg.device = torch.device(cfg.device)
+    # generator = ru.set_deterministic(cfg.seed)
+    # split_dls = du.get_dataloaders(cfg, generator)
+    # model = create_model(cfg, len(split_dls['train'].dataset.classes))
+    # train_loop(cfg, model, split_dls['train'], val_dl=split_dls['val'])
 
-    _add_params(model)
+    criterion = get_criterion(cfg)
+    optim, lr_sched = get_optim(cfg, model)
+    mu.checkpoint_model(cfg, model, "init_model")
 
-    param_groups = []
-    for key in params:
-        if len(params[key]) > 0:
-            param_groups.append({"params": params[key], "weight_decay": params_weight_decay[key]})
-    return param_groups
+    start_time = time.time()
+    for epoch in range(cfg.epochs):
+        print(f">> Start Epoch: {epoch}")
+        
+        # Train
+        tm = train_epoch(cfg, epoch, model, train_dl, criterion, optim)
+        lr_sched.step()
+        tm_str = ' '.join([f"{k}={v}" for k, v in agg_metrics(tm))
+        print(f":: TRAIN :: {tm_str}")
 
+        # Val
+        if val_dl is not None:
+            vm = eval_model(cfg, model, val_dl, criterion)
+            vm_str = ' '.join([f"{k}={v}" for k, v in agg_metrics(vm))
+            print(f":: VAL :: {vm_str}")
+        
+        mu.checkpoint_model(cfg, model, f"epoch_{epoch}")
+        print()
+
+    total_time = time.time() - start_time
+    total_ts = str(datetime.timedelta(seconds=int(total_time)))
+    print(f"Training time {total_ts}")
+    
