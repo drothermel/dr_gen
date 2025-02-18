db8a0ae4fb7997fa0c0adc2374e94cb018a2c844
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-18 12:29:42 -0500

    add md to cfg and test it is usable in script/train

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,11 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
-import logging
+
+from dr_util.config_verification import validate_cfg
+from dr_gen.utils.train_eval import GenMetrics
+from dr_util.schemas import get_schema
+
+from dr_gen.utils import data
 
 import torch
 import dr_gen.run_utils as ru
@@ -9,43 +14,32 @@ import dr_gen.model_utils as mu
 import dr_gen.train_eval as te
 
 
-# Potentially add all of this to dr_util
-def cfg_to_loggable_lines(cfg):
-    if isinstance(cfg, dict):
-        cfg_str = str(cfg)
-    else:
-        resolved_cfg = OmegaConf.to_container(cfg, resolve=True)
-        cfg_str = OmegaConf.to_yaml(resolved_cfg)
-    return cfg_str.strip("\n").split("\n")
-
-
-def log_cfg(cfg):
-    logging.info("=" * 19 + "   Config   " + "=" * 19)
-    for cl in cfg_to_loggable_lines(cfg):
-        logging.info(cl)
-    logging.info("=" * 50)
-    logging.info("")
-
-
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+@hydra.main(version_base=None, config_path="../configs", config_name="config")
 def run(cfg: DictConfig):
-    log_cfg(cfg)
-    logging.info(">> Running Training")
+    if not validate_cfg(cfg, "uses_metrics", get_schema):
+        return
+
+    # Make Metrics and Log Cfg
+    md  = GenMetrics(cfg)
+    md.log(cfg)
+    cfg.md = md
+
+    cfg.md.log(">> Running Training")
 
     # Setup
     cfg.device = torch.device(cfg.device)
     generator = ru.set_deterministic(cfg.seed)
 
     # Data
-    logging.info(" :: Loading Dataloaders :: ")
+    cfg.md.log(" :: Loading Dataloaders :: ")
     split_dls = du.get_dataloaders(cfg, generator)
-    logging.info(f">> Downloaded to: {cfg.paths.dataset_cache_root}")
+    cfg.md.log(f">> Downloaded to: {cfg.paths.dataset_cache_root}")
 
     # Model
     model = mu.create_model(cfg, len(split_dls["train"].dataset.classes))
 
     # Run Train
-    te.train_loop(cfg, model, split_dls["train"], val_dl=split_dls["val"])
+    #te.train_loop(cfg, model, split_dls["train"], val_dl=split_dls["val"])
 
 
 if __name__ == "__main__":
