9a38af0532fb7876939da63b4cab7b53b5f734f5
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-18 12:37:48 -0500

    extract log_metrics back into a fxn in train_eval.py

--- src/dr_gen/utils/train_eval.py
+++ src/dr_gen/utils/train_eval.py
@@ -19,13 +19,11 @@ LR_SCHEDULERS = "steplr", "cosineannealinglr", "exponentiallr"}
 # md.log_data(data, group_name, ns=1)
 
 def log_metrics(cfg, group_name, **kwargs):
-    if cfg.md is None:
-        print(">> WARNING: No metrics object for logging")
-        return
+    assert cfg.md is not None, "There should be a metrics obj"
 
+    loss = kwargs.get("loss", None)
     output = kwargs.get("output", None)
     target = kwargs.get("target", None)
-    loss = kwargs.get("loss", None)
 
     if output is not None:
         cfg.md.log_data((BATCH_KEY, output.shape[0]))
@@ -39,7 +37,7 @@ def log_metrics(cfg, group_name, **kwargs):
         cfg.md.log_data(('loss', loss))
 
 
-def train_epoch(cfg, epoch, model, dataloader, criterion, optimizer, metrics):
+def train_epoch(cfg, epoch, model, dataloader, criterion, optimizer):
     model.train()
     for i, (image, target) in enumerate(dataloader):
         image, target = image.to(cfg.device), target.to(cfg.device)
@@ -53,14 +51,8 @@ def train_epoch(cfg, epoch, model, dataloader, criterion, optimizer, metrics):
                 cfg.optim.clip_grad_norm,
             )
         optimizer.step()
-        acc1, acc5 = eu.accuracy(output, target, topk=(1, 5))
-        metrics.train(
-            {
-                'loss': loss.item(),
-                'acc1': acc1.item(), 
-                'acc5': acc5.item(),
-            },
-            ns=image.shape[0],
+        log_metrics(
+            cfg, 'train', loss=loss, output=output, target=target,
         )
     return metrics
 
@@ -73,41 +65,36 @@ def eval_model(cfg, model, dataloader, criterion, metrics):
             target = target.to(cfg.device, non_blocking=True)
             output = model(image)
             loss = criterion(output, target)
-            acc1, acc5 = eu.accuracy(output, target, topk=(1, 5))
-            metrics.val(
-                {
-                    'loss': loss.item(),
-                    'acc1': acc1.item(), 
-                    'acc5': acc5.item(),
-                },
-                ns=image.shape[0],
+            log_metrics(
+                cfg, 'val', loss=loss, output=output, target=target,
             )
     return metrics
 
 
 def train_loop(cfg, model, train_dl, val_dl=None):
+    assert cfg.md is not None
     criterion = mu.get_criterion(cfg)
     optim, lr_sched = mu.get_optim(cfg, model)
     mu.checkpoint_model(cfg, model, "init_model")
 
     start_time = time.time()
     for epoch in range(cfg.epochs):
-        md = GenMetrics(cfg)
-        md.log(f">> Start Epoch: {epoch}")
+        cfg.md.log(f">> Start Epoch: {epoch}")
 
         # Train
         md = train_epoch(cfg, epoch, model, train_dl, criterion, optim, md)
         lr_sched.step()
-        md.agg_log("train")
+        cfg.md.agg_log("train")
 
         # Val
         if val_dl is not None:
             md = eval_model(cfg, model, val_dl, criterion, md)
-            md.agg_log("val")
+            cfg.md.agg_log("val")
 
         mu.checkpoint_model(cfg, model, f"epoch_{epoch}")
-        print()
+        cfg.md.clear_data()
+        cfg.md.log("")
 
     total_time = time.time() - start_time
     total_ts = str(datetime.timedelta(seconds=int(total_time)))
-    print(f"Training time {total_ts}")
+    cfg.md.log(f"Training time {total_ts}")
