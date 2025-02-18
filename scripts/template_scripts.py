18efb7489fb61b733a456f04e4043343ec69f35d
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-17 23:06:48 -0500

    move confs to ../configs, add metrics to cfg

--- scripts/template_scripts.py
+++ scripts/template_scripts.py
@@ -1,6 +1,5 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
-import logging
 
 from dr_util.config_verification import validate_cfg
 from dr_util.metrics import Metrics
@@ -9,7 +8,7 @@ from dr_util.schemas import get_schema
 from dr_gen.utils import data
 
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+@hydra.main(version_base=None, config_path="../configs", config_name="config")
 def run(cfg: DictConfig):
     if not validate_cfg(cfg, "uses_metrics", get_schema):
         return
@@ -17,16 +16,7 @@ def run(cfg: DictConfig):
     # Make Metrics and Log Cfg
     md  = Metrics(cfg)
     md.log(cfg)
-
-    """
-    log_cfg(cfg)
-    logging.info(">> Welcome to your new script! here")
-
-    logging.info(" :: Loading Train Dataset :: ")
-    data.get_dataset(cfg.data, cfg.paths, train=True)
-    data.get_dataset(cfg.data, cfg.paths, train=False)
-    logging.info(f">> Downloaded to: {cfg.paths.dataset_cache_root}")
-    """
+    md.log(">> Welcome to your new script!")
 
 
 if __name__ == "__main__":
