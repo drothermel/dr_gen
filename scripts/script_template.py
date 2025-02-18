1579590ff682ce86905db0ebdf6ef28c35447a02
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-17 22:38:17 -0500

    add the dr_util Metrics to script_template.py

--- scripts/script_template.py
+++ scripts/script_template.py
@@ -2,29 +2,23 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import logging
 
+from dr_util.config_verification import validate_cfg
+from dr_util.metrics import Metrics
+from dr_util.schemas import get_schema
+
 from dr_gen.utils import data
-
-
-# Potentially add all of this to dr_util
-def cfg_to_loggable_lines(cfg):
-    if isinstance(cfg, dict):
-        cfg_str = str(cfg)
-    else:
-        resolved_cfg = OmegaConf.to_container(cfg, resolve=True)
-        cfg_str = OmegaConf.to_yaml(resolved_cfg)
-    return cfg_str.strip("\n").split("\n")
-
-
-def log_cfg(cfg):
-    logging.info("=" * 19 + "   Config   " + "=" * 19)
-    for cl in cfg_to_loggable_lines(cfg):
-        logging.info(cl)
-    logging.info("=" * 50)
-    logging.info("")
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def run(cfg: DictConfig):
+    if not validate_cfg(cfg, "uses_metrics", get_schema):
+        return
+
+    # Make Metrics and Log Cfg
+    md  = Metrics(cfg)
+    md.log(cfg)
+
+    """
     log_cfg(cfg)
     logging.info(">> Welcome to your new script! here")
 
@@ -32,6 +26,7 @@ def run(cfg: DictConfig):
     data.get_dataset(cfg.data, cfg.paths, train=True)
     data.get_dataset(cfg.data, cfg.paths, train=False)
     logging.info(f">> Downloaded to: {cfg.paths.dataset_cache_root}")
+    """
 
 
 if __name__ == "__main__":
