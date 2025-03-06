932a1b57d0eddf5276ce6e22836a1f395d9a8a4d
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-03-06 17:43:55 -0600

    add in summary plots and grids

--- src/dr_gen/analyze/plot_utils.py
+++ src/dr_gen/analyze/plot_utils.py
@@ -79,6 +79,41 @@ def format_plot_element(plc, ax):
         ax.set_ylim(plc.ylim)
     ax.grid(plc.grid)
 
+def get_multi_curve_summary_stats(data_list):
+    data_list = make_list_of_lists(data_list)
+    curve_len = len(data_list[0])
+    assert all([len(dl) == curve_len for dl in data_list]), (
+        ">> All curves must be same length"
+    )
+
+    n = len(data_list)
+    v_array = np.array(data_list)
+    v_mean = np.mean(data_list, axis=0)
+    v_std = np.std(v_array, axis=0)
+    v_sem = v_std / np.sqrt(n)
+    all_stats = {
+        "n": n,
+        "x_vals": list(range(curve_len)),
+        "mean": v_mean,
+        "std": v_std,
+        "sem": v_sem,
+        "min": np.min(v_array, axis=0),
+        "max": np.max(v_array, axis=0),
+    }
+    return {
+        k: v.tolist() if isinstance(v, np.ndarray) else v for k, v in all_stats.items()
+    }
+
+def extract_color(ax, total=None, ind=None):
+    lines = ax.get_lines()
+    if len(lines) == 0:
+        return None
+
+    if ind is None or (total is None and len(lines) < ind) or len(lines) < total:
+        return lines[-1].get_color()
+
+    return lines[-total:][ind].get_color()
+
 # ----------------- Add Elements ------------------------
 
 def add_lines_to_plot(plc, ax, data_list, xs=None):
@@ -95,6 +130,89 @@ def add_lines_to_plot(plc, ax, data_list, xs=None):
             linewidth=plc.linewidth,
             label=plc.labels[i],
         )
+
+def add_min_max_shade_to_plot(
+    plc, ax, data_list,
+    data_stats=None,
+    min_line=False, max_line=False,
+):
+    data_list = make_list_of_lists(data_list)
+    data_n = len(data_list)
+
+    if data_stats is None:
+        data_stats = [
+            get_multi_curve_summary_stats(dl) for dl in data_list
+        ]
+
+    colors = [extract_color(ax, total=data_n, ind=i) for i in  range(data_n)]
+
+    for i, dl in enumerate(data_list):
+        dstats = data_stats[i]
+        x = dstats['x_vals']
+        ax.fill_between(
+            x, dstats["min"], dstats["max"], color=colors[i], alpha=0.1
+        )
+        if min_line:
+            ax.plot(x, dstats['min'], color=colors[i], alpha=0.6)
+        if max_line:
+            ax.plot(x, dstats['max'], color=colors[i], alpha=0.6)
+
+def add_std_shade_to_plot(
+    plc, ax, data_list,
+    data_stats=None,
+    std_min_line=False, std_max_line=False,
+):
+    data_list = make_list_of_lists(data_list)
+    data_n = len(data_list)
+
+    if data_stats is None:
+        data_stats = [
+            get_multi_curve_summary_stats(dl) for dl in data_list
+        ]
+
+    colors = [extract_color(ax, total=data_n, ind=i) for i in  range(data_n)]
+    for i, dl in enumerate(data_list):
+        dstats = data_stats[i]
+        x = dstats['x_vals']
+        std_low = [m - s for m, s in zip(dstats['mean'], dstats['std'])]
+        std_high = [m + s for m, s in zip(dstats['mean'], dstats['std'])]
+        ax.fill_between(
+            x,  std_low, std_high,
+            color=colors[i], alpha=0.3
+        )
+        if std_min_line:
+            ax.plot(x, std_low, color=colors[i], alpha=0.6)
+        if std_max_line:
+            ax.plot(x, std_high, color=colors[i], alpha=0.6)
+
+def add_sem_shade_to_plot(
+    plc, ax, data_list,
+    data_stats=None,
+    sem_min_line=False, sem_max_line=False,
+):
+    data_list = make_list_of_lists(data_list)
+    data_n = len(data_list)
+
+    if data_stats is None:
+        data_stats = [
+            get_multi_curve_summary_stats(dl) for dl in data_list
+        ]
+
+    colors = [extract_color(ax, total=data_n, ind=i) for i in  range(data_n)]
+    for i, dl in enumerate(data_list):
+        dstats = data_stats[i]
+        x = dstats['x_vals']
+        sem_low = [m - s for m, s in zip(dstats['mean'], dstats['sem'])]
+        sem_high = [m + s for m, s in zip(dstats['mean'], dstats['sem'])]
+        ax.fill_between(
+            x,  sem_low, sem_high,
+            color=colors[i], alpha=0.3
+        )
+        if sem_min_line:
+            ax.plot(x, sem_low, color=colors[i], alpha=0.6)
+        if sem_max_line:
+            ax.plot(x, sem_high, color=colors[i], alpha=0.6)
+
 
 # ----------------- Make Plots ------------------------
 
@@ -114,6 +232,70 @@ def make_line_plot(data_lists, ax=None, **kwargs):
     if fig is not None:
         plt.show()
 
+
+def make_summary_plot(data_lists, ax=None, **kwargs):
+    assert len(data_lists) > 0, ">> Empty data lists"
+    n_data = len(data_lists)
+    if 'plc' in kwargs:
+        plc = kwargs['plc']
+    else:
+        plc = get_plt_cfg(**kwargs)
+
+    fig = None
+    if ax is None:
+        fig, ax = plt.subplots(figsize=plc.figsize)
+
+    data_stats = [
+        get_multi_curve_summary_stats(dl) for dl in data_lists
+    ]
+
+    # Mean Line
+    if plc.labels is None or len(plc.labels) != n_data:
+        plc.labels = [f"{i}" for i in range(n_data)]
+    orig_linewidth = plc.linewidth
+    plc.linewidth = kwargs.get("mean_linewidth", 2)
+    plc.labels = [
+        f"{lb} Mean (#seeds: {len(data_lists[i])})" for i, lb in enumerate(plc.labels)
+    ]
+    add_lines_to_plot(
+        plc, ax, [ds['mean'] for ds in data_stats],
+    )
+    plc.linewidth = orig_linewidth
+
+    # Min Max Shade
+    if kwargs.get("min_max_shade", True):
+        plc.labels = [None for _ in range(n_data)]
+        add_min_max_shade_to_plot(
+            plc, ax, data_lists,
+            data_stats=data_stats,
+            min_line=kwargs.get('min_line', False),
+            max_line=kwargs.get('max_line', False),
+        )
+
+    if kwargs.get("std_shade", True):
+        plc.labels = [None for _ in range(n_data)]
+        add_std_shade_to_plot(
+            plc, ax, data_lists,
+            data_stats=data_stats,
+            std_min_line=kwargs.get('std_min_line', False),
+            std_max_line=kwargs.get('std_max_line', False),
+        )
+
+    if kwargs.get("sem_shade", True):
+        plc.labels = [None for _ in range(n_data)]
+        add_sem_shade_to_plot(
+            plc, ax, data_lists,
+            data_stats=data_stats,
+            sem_min_line=kwargs.get('sem_min_line', False),
+            sem_max_line=kwargs.get('sem_max_line', False),
+        )
+
+    format_plot_element(plc, ax)
+    if fig is not None:
+        plt.show()
+
+
+    
 
 # ----------------- Make Grids ------------------------
 
@@ -150,7 +332,7 @@ def grid_wrapper(plot_func, data_lists, **kwargs):
 
     # Plot data
     for i, (ax, data_list) in enumerate(zip(axes.flatten(), data_lists)):
-        plot_func(data_list, ax=ax, plc=plc_list[i])
+        plot_func(data_list, ax=ax, plc=plc_list[i], **kwargs)
 
     # Annotate Plot
     if grid_plc.subplot_ylabel is not None:
@@ -170,75 +352,9 @@ def grid_wrapper(plot_func, data_lists, **kwargs):
 # =============================================================
 
 # -----------------------------------------------------------
-#               Misc & Calc Plot Elements
-# -----------------------------------------------------------
-
-
-def get_multi_curve_summary_stats(data_list):
-    data_list = make_list_of_lists(data_list)
-    curve_len = len(data_list[0])
-    assert all([len(dl) == curve_len for dl in data_list]), (
-        ">> All curves must be same length"
-    )
-
-    n = len(data_list)
-    v_array = np.array(data_list)
-    v_mean = np.mean(data_list, axis=0)
-    v_std = np.std(v_array, axis=0)
-    v_sem = v_std / np.sqrt(n)
-    all_stats = {
-        "n": n,
-        "x_vals": list(range(curve_len)),
-        "mean": v_mean,
-        "std": v_std,
-        "sem": v_sem,
-        "std_lower": v_mean - v_std,
-        "std_upper": v_mean + v_std,
-        "sem_lower": v_mean - v_sem,
-        "sem_upper": v_mean + v_sem,
-        "min": np.min(v_array, axis=0),
-        "max": np.max(v_array, axis=0),
-    }
-    return {
-        k: v.tolist() if isinstance(v, np.ndarray) else v for k, v in all_stats.items()
-    }
-
-
-# -----------------------------------------------------------
 #                  Add Elems to Plot
 # -----------------------------------------------------------
 
-
-def format_plot_grid(plc):
-    if plc.xlabel is not None:
-        plt.xlabel(plc.xlabel)
-    if plc.ylabel is not None:
-        plt.ylabel(plc.ylabel)
-    if plc.title is not None:
-        plt.title(plc.title)
-    if plc.legend:
-        plt.legend()
-    if plc.xlim is not None:
-        plt.xlim(plc.xlim)
-    if plc.ylim is not None:
-        plt.ylim(plc.ylim)
-    plt.grid(plc.grid)
-
-
-def add_lines_to_plot_old(plc, data_list, xs=None):
-    data_list = make_list_of_lists(data_list)
-    plc = init_plc_lists(plc, len(data_list))
-
-    if xs is None:
-        xs = range(len(data_list[0]))
-    for i, data in enumerate(data_list):
-        plt.plot(
-            xs,
-            data,
-            linestyle=plc.linestyle,
-            linewidth=plc.linewidth,
-            label=plc.labels[i],
-        )
 
 
 def add_cdfs_to_plot(plc, vals, cdfs):
