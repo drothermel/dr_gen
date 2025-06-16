3be82d78e5afbbfb55ffd3b3b860da87c5be723a
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-06-16 23:43:50 +0100

    fix: rename import functional as F to f to follow naming conventions

--- src/dr_gen/data/viz.py
+++ src/dr_gen/data/viz.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
-from torchvision.transforms.v2 import functional as F
+from torchvision.transforms.v2 import functional as f
 
 
 def plot_first_from_dl(dl):
@@ -24,14 +24,14 @@ def plot(imgs, row_title=None, **imshow_kwargs):
         for col_idx, img in enumerate(row):
             if isinstance(img, tuple):
                 img, target = img
-            img = F.to_image(img)
+            img = f.to_image(img)
             if img.dtype.is_floating_point and img.min() < 0:
                 # Poor man's re-normalization for the colors to be OK-ish. This
                 # is useful for images coming out of Normalize()
                 img -= img.min()
                 img /= img.max()
 
-            img = F.to_dtype(img, torch.uint8, scale=True)
+            img = f.to_dtype(img, torch.uint8, scale=True)
 
             ax = axs[row_idx, col_idx]
             ax.imshow(img.permute(1, 2, 0).numpy(), **imshow_kwargs)
